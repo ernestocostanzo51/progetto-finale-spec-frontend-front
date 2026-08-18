62cc27eb54bc079d9f633e1b65f12e31d52be2ee
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-08-18 17:51:09 +0200

    creazione GamesProvider

--- src/components/GamesProvider.jsx
+++ src/components/GamesProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react"
+import { createContext, useState, useEffect } from "react"
 
 const GamesContext = createContext()
 
@@ -6,18 +6,38 @@ const GamesContext = createContext()
 
 export default function ProductsProvider({ childern }){
 
+    const [games , setGames] = useState([])
+
     const FetchGames = async() => {
     try{
         const response = await fetch("http://localhost:3001/products/")
         if(!response.ok){
-            throw new Error (response.status)
+            throw new Error (`errore http ${response.status}`)
         }
 
+        const data = await response.json()
+        setGames(data)
 
-    }catch{
+
+    }catch(error){
+        if(error instanceof Error){
+            console.error(error)
+        }
+        else{
+            console.error
+        }
 
     }
 }
+
+return(
+    <GamesContext.Provider value={
+        games,
+        setGames,
+        FetchGames
+    }
+    ></GamesContext.Provider>
+)
     
        
     
